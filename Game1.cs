56992c8e47debb9223ab2ca1970c8a18e6a8e739
Afonso <afonasmiranda@gmail.com>
2026-05-15 16:57:22 +0100

    Add fullscreen mode with F11 toggle

--- Game1.cs
+++ Game1.cs
@@ -2,43 +2,50 @@
 using BurnoutCity.Entities;
 using BurnoutCity.States;
 using Microsoft.Xna.Framework;
-
-
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
 namespace BurnoutCity;
 
 public class Game1 : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch = null!;
-
     private GameStateManager _stateManager = null!;
 
     public static Texture2D PixelTexture { get; private set; } = null!;
     public static System.Action? QuitGame { get; private set; }
+
     public const int ScreenWidth = 1280;
     public const int ScreenHeight = 720;
+
+    private KeyboardState _previousKeyboardState;
+
     public Game1()
     {
         _graphics = new GraphicsDeviceManager(this)
         {
             PreferredBackBufferWidth = ScreenWidth,
             PreferredBackBufferHeight = ScreenHeight,
-            IsFullScreen = false
+            IsFullScreen = true
         };
+
         Content.RootDirectory = "Content";
         IsMouseVisible = true;
+
         Window.Title = "Burnout City";
+
         QuitGame = Exit;
+
+        _graphics.ApplyChanges();
     }
 
     protected override void Initialize()
     {
-
-        //Criar o GameStateManager e inicializar o estado inicial do jogo 
         _stateManager = new GameStateManager();
-_stateManager.Initialize(GraphicsDevice, Content);
-_stateManager.ChangeState(new MenuState());
+        _stateManager.Initialize(GraphicsDevice, Content);
+        _stateManager.ChangeState(new MenuState());
+
         base.Initialize();
     }
 
@@ -46,21 +53,45 @@ _stateManager.ChangeState(new MenuState());
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-        PixelTexture = new Texture2D(GraphicsDevice, 1, 1); //Criar uma textura de 1x1 pixel para desenhar formas simples
-        PixelTexture.SetData(new[] { Color.White }); //Definir a cor do pixel para branco, permitindo que seja tintada para outras cores ao desenhar
+        PixelTexture = new Texture2D(GraphicsDevice, 1, 1);
+
+        PixelTexture.SetData(new[] { Color.White });
     }
+
     protected override void Update(GameTime gameTime)
     {
+        KeyboardState keyboardState = Keyboard.GetState();
+
+        // Toggle fullscreen com F11
+        if (keyboardState.IsKeyDown(Keys.F11) &&
+            !_previousKeyboardState.IsKeyDown(Keys.F11))
+        {
+            ToggleFullscreen();
+        }
+
+        _previousKeyboardState = keyboardState;
+
         _stateManager.Update(gameTime);
+
         base.Update(gameTime);
     }
+
+    private void ToggleFullscreen()
+    {
+        _graphics.IsFullScreen = !_graphics.IsFullScreen;
+        _graphics.ApplyChanges();
+    }
+
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(new Color(15, 15, 20)); //Limpar a tela com uma cor de fundo escura
-        _spriteBatch.Begin(); //Iniciar o SpriteBatch para desenhar os elementos do jogo
-        _stateManager.Draw(_spriteBatch); //Desenhar o estado ativo do jogo usando o SpriteBatch
-        _spriteBatch.End(); //Finalizar o SpriteBatch após desenhar os elementos do jogo
-        base.Draw(gameTime); //Chamar o método base para garantir que outros elementos do jogo sejam desenhados corretamente
+        GraphicsDevice.Clear(new Color(15, 15, 20));
+
+        _spriteBatch.Begin();
+
+        _stateManager.Draw(_spriteBatch);
+
+        _spriteBatch.End();
+
+        base.Draw(gameTime);
     }
-    
 }
